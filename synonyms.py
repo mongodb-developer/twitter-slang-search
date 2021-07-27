03703a292e9c06d1f7448fdcc0b884be58818ab2
mongoanaiya <anaiya.raisinghani@mongodb.com>
2021-07-27 11:57:58 -0700

    first synonym push

---
+++ synonyms.py
@@ -0,0 +1,397 @@
+# -*- coding: utf-8 -*-
+
+#This is the textfile for synonyms project"
+#need to figure out how to incorporate this into Atlas Search
+
+# using the equivalent mapping type so that we can retrieve slang 
+# if we use a synonym of slang, decided to not use the explicit mapping type 
+# because I want the mappings to be bidirectional so that we can get better results 
+
+
+#these are the documents for 
+
+
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["basic", "regular", "boring"]
+ 
+ }
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["bet", "agree", "concur"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["yikes", "embarrassing", "bad", "awkward"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["fam", "family", "friends"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["sheesh", "bragging", "cringe", "excitement"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["cancelled", "done", "dead", "irrelevant"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["cap", "lie", "false"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["drip", "cool style", "fashionable", "brands", "expensive"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["drag","insult"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["pressed", "angry", "upset", "frustrated"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["butthurt", "annoyed", "irritated", "upset"]
+ 
+ } 
+
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["slaps", "enjoyable", "good"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["smacks", "delicious","yummy"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["snatched", "skinny", "fit"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["stan", "fan", "admirer", "idol", "icon"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["sus", "suspicious", "skeptical", "distrustful"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["tea", "gossip", "scandal", "tattle"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["vibe", "energy", "aura"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["yeet", "throw", "agree"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["flex", "flaunt", "parade", "boast", "brag"]
+ 
+ } 
+
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["cheugy","basic","cheap", "old" ]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["ghosted", "ignore"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["squad", "group", "friends"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["skrt", "fast", "speeding", "evade", "dodge"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["shook", "scared", "confused"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["salty", "upset","hurt", "angry"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["woke", "understanding", "cultured"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["mood", "agreeable", "understandable"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["receipts", "proof", "evidence", "confirmation"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["beat", "makeup", "ugly"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["swerve", "evade", "ignore"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["slay", "pretty"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["shade", "sneaky", "insult"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["bae", "boyfriend", "girlfriend", "partner"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["lit", "excited", "drunk", "animated", "exuberant"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["thirsty", "desperate","discouraged", "promiscuous"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["fit", "outfit","attractive","appealing"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["fire", "attractive", "appealing", "captivating"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["lowkey", "muted","restrained","subtle"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["highkey", "established","intense", "clearly"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["sksksk", "excitement", "enthusiasm", "eagerness"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["simp", "kind", "admire", "infatuated"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["clout", "fashionable", "influence", "power"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["banger", "enjoyable"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["boujee", "fancy", "expensive", "showy"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["dank", "delicious", "yummy", "strong"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["finna", "gonna"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["guap", "money"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["hype", "cool", "excited"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["swole", "buffed", "ripped"]
+ 
+ } 
+
+{
+ 
+ "mappingType": "equivalent",
+ "synonyms": ["chile","chill","child" ]
+ 
+ } 
+
+
+#dynamic mapping to configure an index for the synsearch.synonyms collection
+
+{
+ 
+ "mappings": {
+     "dynamic": true
+     },
+ "synonyms": [
+     {
+      "analyzer":"lucene.standard",
+      "name": "synonyms",
+      "source": {
+          "collection": "tweets"
+          }
+      }
+     ]
+ }
+
+
+
+
+
+
